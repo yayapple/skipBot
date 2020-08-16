bc326c4920e1103ce2df98f9eade5364051985cf
dumbass <57227052+gitlabsux@users.noreply.github.com>
2020-08-16 03:37:37 -0500

    get bot running

---
+++ main.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix = '[')
+
+@bot.command
+async def ping(ctx):
+    await ctx.send('pong')
+
+bot.run(os.getenv('TOKEN'))
