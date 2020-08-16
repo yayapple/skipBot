9cf1090e150d24984b94985e43d57153691b395e
dumbass <57227052+gitlabsux@users.noreply.github.com>
2020-08-16 04:03:00 -0500

    forgot to add the cog

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ bot = commands.Bot(command_prefix = '[')
 async def ping(ctx):
 	await ctx.send('pong')
 
+@bot.command(aliases=['l'])
+async def load(ctx, extension):
+	if not ctx.author.id == 290983149507182592:
+		return
+	bot.load_extension(f'cogs.{extension}')
+	await ctx.send(f'loaded {extension}')
+
+@bot.command(aliases=['ul', 'u'])
+async def unload(ctx, extension):
+	if not ctx.author.id == 290983149507182592:
+		return
+	bot.unload_extension(f'cogs.{extension}')
+	await ctx.send(f'unloaded {extension}')
+
+
+for filename in os.listdir('./cogs'):
+	if filename.endswith('.py') and filename not in ['import.py']:
+		bot.load_extension(f'cogs.{filename[:-3]}')
+
 
 @bot.event
 async def on_ready():
